53e73bd6dddf976c1a93265a09ba2ea24902b5d0
Crispioso <crispin.merriman@gmail.com>
2017-01-17 15:23:09 +0000

    Rename client-side code directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-	entry: './src/index.js',
+	entry: './client/index.js',
 	output: {
 		path: './dist',
 		filename: 'bundle.js',
