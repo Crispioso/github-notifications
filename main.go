e83fbea287743b8b873b5eba8681b790dbbd7c72
Crispioso <crispin.merriman@gmail.com>
2017-01-31 09:58:54 +0000

    Ability to use custom filters - still to add some validation

--- main.go
+++ main.go
@@ -79,22 +79,25 @@ func listNotifications(w http.ResponseWriter, req *http.Request) {
 	//
 	//}
 
+	log.Printf("Request for filter %s", filterName)
+
 	if len(filter) == 0 {
-		var customFilter []map[string]interface{}
-		err := filters.Find(bson.M{"slug": filterName}).All(&customFilter)
+		filters = session.DB("github-notifications").C("filters")
+		var customFilter map[string]interface{}
+		err := filters.Find(bson.M{"slug": filterName}).One(&customFilter)
 		if err != nil {
-			log.Println(err)
+			log.Printf("Unable to find custom filter '%s'", filterName)
 			w.WriteHeader(500)
 			return
 		}
 
-		//log.Printf("Custom filter: %s", customFilter)
-
-		//filter = customFilter["queries"]
+		filter = make(map[string]interface{})
+		for k, v := range customFilter["queries"].(map[string]interface{}) {
+			filter[k] = v
+		}
 	}
 
-	log.Printf("Filter found: \n%s", filter)
-	log.Printf("Request for filter %s", filterName)
+	log.Printf("Filter: %s", filter)
 
 	err := notifications.Find(filter).All(&notificationsResponse)
 	if err != nil {
